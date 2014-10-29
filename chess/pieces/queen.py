c246eecc0bdc0dd9bf6500d4b7e9e1a84630e3d1
Joe D. Bentley <joebentley10@gmail.com>
2014-10-29 01:01:47 +0000

    Added queen movement

--- chess/pieces/queen.py
+++ chess/pieces/queen.py
@@ -9,6 +9,15 @@ class Queen(Piece):
             return 'q'
 
     def validMove(self, position):
-        # Check for diagonal movement
-        pass
+        # Check for diagonal movement (for diagonal movement, dy/dx = 1)
+        if abs(position.y - self.position.y) / abs(position.x - self.position.x) == 1:
+            return True
+
+        # Else check for horizontal and vertical linear movement
+        if ((abs(position.x - self.position.x) > 0 and (position.y - self.position.y == 0)) or
+            (abs(position.y - self.position.y) > 0 and (position.x - self.position.x == 0))):
+
+            return True
+
+        return False
 
