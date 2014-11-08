fb644150de15f7f77fe1159b3bfe05ff71d7b8a9
Joe D. Bentley <joebentley10@gmail.com>
2014-11-08 16:21:27 +0000

    Fixed typo in move checking.

--- chess/move.py
+++ chess/move.py
@@ -15,7 +15,7 @@ def check_move(board, from_pos, to_pos):
     if not piece.valid_move(to_pos) or not board.point_on_board(to_pos):
         return False
     # Check if another piece at destination and it's the same colour, the move is invalid
-    if board.piece_at(to_pos) and board.getPiece(to_pos).color == piece.color:
+    if board.piece_at(to_pos) and board.getsquare(to_pos).color == piece.color:
         return False
     # Check if there are any pieces in the path of our piece
     if piece.is_path_clear(board, to_pos):
