2cee044aa3afbf024e2d429ee9a93dfa03ddfad2
Joe D. Bentley <joebentley10@gmail.com>
2014-11-09 17:59:18 +0000

    Finished Rook movement (except for castling)

--- chess/pieces/rook.py
+++ chess/pieces/rook.py
@@ -1,5 +1,6 @@
 from chess.pieces.color import Color
 from chess.pieces.piece import Piece
+from chess.point import Point
 
 class Rook(Piece):
     def render(self):
@@ -8,12 +9,37 @@ class Rook(Piece):
         else:
             return 'r'
 
-    def valid_move(self, position):
-        # Check for horizontal and vertical movement
-        if ((abs(position.x - self.position.x) > 0 and (position.y - self.position.y == 0)) or
-            (abs(position.y - self.position.y) > 0 and (position.x - self.position.x == 0))):
+    def valid_move(self, board, position):
+        # Check if movement isn't horizontal or vertical
+        if not (((abs(position.x - self.position.x) > 0 and
+                 (position.y - self.position.y == 0)) or
+                 (abs(position.y - self.position.y) > 0 and
+                 (position.x - self.position.x == 0)))):
 
-            return True
+            return False
 
-        return False
+
+        # Check if there is a piece between the rook's position and its destination.
+
+        # If the position delta is negative we need to range upwards,
+        # delta/abs(delta) will give us a step of 1 or -1 depending on
+        # whether or not the position delta is positive or negative
+        y_delta = position.y - self.position.y
+        x_delta = position.x - self.position.x
+
+        if y_delta > 0:
+            step_y = int(y_delta/abs(y_delta))
+
+            # Start at 1 or -1, not 0, this is the same as the step_y
+            for y in range(step_y, y_delta, step_y):
+                if board.piece_at(Point(self.position.x, self.position.y + y)):
+                    return False
+
+        if x_delta > 0:
+            step_x = int(x_delta/abs(x_delta))
+            for x in range(step_x, x_delta, step_x):
+                if board.piece_at(Point(self.position.x + x, self.position.y)):
+                    return False
+
+        return True
 
